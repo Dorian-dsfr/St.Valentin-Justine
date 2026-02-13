eff212ed41d7b02accf281dba7c926935974ca1c
Dorian Desfour <dorian.desfour.pro@gmail.com>
2026-02-13 21:15:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
-})
-
-
-// https://vitejs.dev/config/
-export default defineConfig({
   plugins: [react()],
 })
